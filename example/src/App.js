618c1a4d57ce299eaaa1c57b04ab354f47df5890
Pablo Isaac Lopez Jaimes <lopezpabj@globalhitss.com>
2021-01-28 20:29:34 -0600

    manage events and call urls

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,66 @@
-import React from 'react'
+import React from 'react';
+import PlayerEvents from 'player-events';
 
-import { ExampleComponent } from 'player-events'
-import 'player-events/dist/index.css'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://servicios.site/BLDash/ads.json',
+      action: 'midpoint'
+    }
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+    this.handleChangeUrl = this.handleChangeUrl.bind(this);
+    this.handleChangeAction = this.handleChangeAction.bind(this);
+    this.handleCallEvets = this.handleCallEvets.bind(this);
+  }
+
+  handleChangeUrl(e) {
+    this.setState({ url: e.target.value })
+  }
+
+  handleChangeAction(e) {
+    this.setState({ action: e.target.value });
+  }
+
+  handleCallEvets() {
+    console.log(this.state);
+    const events = new PlayerEvents();
+    events.callAllEventsUrl(
+      this.state.url,
+      this.state.action
+    );
+  }
+
+  render() {
+    return (
+      <div className="main-content">
+        <form>
+          <div className="inputUrl">
+            <label htmlFor="adsJson">Url:</label>
+            <input type="text" id="adsJson" onChange={this.handleChangeUrl} value="https://servicios.site/BLDash/ads.json" />
+          </div>
+          <div>
+            <label htmlFor="actiontype">Action:</label>
+            <select id="actiontype" onChange={this.handleChangeAction}>
+              <option value="midpoint">midpoint</option>
+              <option value="firstQuartile">firstQuartile</option>
+              <option value="thirdQuartile">thirdQuartile</option>
+              <option value="unmute">unmute</option>
+              <option value="complete">complete</option>
+              <option value="fullscreen">fullscreen</option>
+              <option value="impression">impression</option>
+              <option value="pause">pause</option>
+              <option value="start">start</option>
+              <option value="mute">mute</option>
+            </select>
+          </div>
+          <div className="controls">
+            <button type="button" onClick={this.handleCallEvets}>CallUrls</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
-import React from 'react'
-import styles from './styles.module.css'
+class PlayerEvents {
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  async getUrl(url) {
+    const res = await fetch(url)
+      .then(res => res)
+      .then(data => data);
+    return res;
+  }
+
+  async readJson(url) {
+    return await fetch(url,
+      {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    ).then(response => response.json())
+      .then(data => data)
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  callUrl(data) {
+    data.map(item => {
+      item.forEach(async element => {
+        const res = await this.getUrl(element);
+        console.log(res);
+      });
+    });
+  }
+
+  searcgEvents(eventType, data) {
+    let urls = [];
+    data.avails.map(item => {
+      let findUrl = item.ads[0].trackingEvents.filter(item => {
+        return item.eventType === eventType;
+      });
+      urls.push(findUrl[0].beaconUrls);
+    });
+
+    return urls;
+  }
+
+  async callAllEventsUrl(url, eventType) {
+    const data = await this.readJson(url);
+    let urls = this.searcgEvents(eventType, data);
+    this.callUrl(urls);
+  }
 }
+
+export default PlayerEvents;
