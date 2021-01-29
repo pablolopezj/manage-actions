61cdac2acae4183bf73770d32078224d0e9c70da
Pablo Isaac Lopez Jaimes <lopezpabj@globalhitss.com>
2021-01-28 20:50:30 -0600

    Updates

---
+++ precache-manifest.dea014fa50d227ccb30a28869828dcfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45d00bdaa4fe3cf6f2c6dfe6d19fdd1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b0052ea54afec5a567d",
+    "url": "./static/css/main.e4061ab2.chunk.css"
+  },
+  {
+    "revision": "d758d0f71404bd1bd7ef",
+    "url": "./static/js/2.812296c5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.812296c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0052ea54afec5a567d",
+    "url": "./static/js/main.1f9cb157.chunk.js"
+  },
+  {
+    "revision": "d950a1a16b4f61b24b41",
+    "url": "./static/js/runtime-main.bbad39c9.js"
+  }
+]);
